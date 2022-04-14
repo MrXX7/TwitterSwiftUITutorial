1ff6485a89c8f4157f8af601625ef442f6b22c90
Oncu <oohancan@gmail.com>
2022-04-14 23:22:11 +0300

    edit registration and image picker

--- TwitterSwiftUITutorial/Utills/Views/ImagePicker.swift
+++ TwitterSwiftUITutorial/Utills/Views/ImagePicker.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
     
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var mode
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
     func makeUIViewController(context: Context) -> some UIViewController {
         let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
         return picker
     }
     
@@ -32,6 +36,9 @@ extension ImagePicker {
             self.parent = parent
         }
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            parent.image = image
+            parent.mode.wrappedValue.dismiss()
         }
     }
 }
