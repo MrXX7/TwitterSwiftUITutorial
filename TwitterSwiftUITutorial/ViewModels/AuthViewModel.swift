fec3098a11b5e2457d32852347ba07a6513cea00
Oncu <oohancan@gmail.com>
2022-04-18 15:22:25 +0300

    edit and add storage authviewmodel

--- TwitterSwiftUITutorial/ViewModels/AuthViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/AuthViewModel.swift
@@ -16,12 +16,39 @@ class AuthViewModel: ObservableObject {
     
     func registerUser(email: String, password: String, username: String, fullname: String, profileImage: UIImage) {
         
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(filename)
+        
+        storageRef.putData(imageData, metadata: nil) { _, error in
             if let error = error {
-            print("DEBUG: Error \(error.localizedDescription)")
-            return
+                print("DEBUG: Failed to upload image \(error.localizedDescription)")
+                return
+            }
+            
+            print("DEBUG: Succesfully upload user photo..")
+            
+            storageRef.downloadURL { url, _ in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                    print("DEBUG: Error \(error.localizedDescription)")
+                    return
+            }
+                    guard let user = result?.user else { return }
+                    
+                    let data = ["email": email,
+                                "username": username,
+                                "fullname": fullname,
+                                "profileImageUrl": profileImageUrl,
+                                "uid": user.uid]
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                        print("DEBUG: Succesfully upload user data..")
+                    }
         }
-            print("DEBUG: Successfully signed up user..")
+        }
+            
     }
 }
 }
